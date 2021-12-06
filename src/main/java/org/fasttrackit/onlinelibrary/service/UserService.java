3a5db638ef45bbf5ab4f5ad05646a25658576ca6
andreibundas <andreibundas@gmail.com>
2021-12-06 23:08:09 +0200

    added updateUser and deleteUser methods in UserService

--- src/main/java/org/fasttrackit/onlinelibrary/service/UserService.java
+++ src/main/java/org/fasttrackit/onlinelibrary/service/UserService.java
@@ -6,6 +6,7 @@ import org.fasttrackit.onlinelibrary.persistence.UserRepository;
 import org.fasttrackit.onlinelibrary.transfer.SaveUserRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,11 +32,25 @@ public class UserService {
     }
 
     public User getUser(long id) {
-        LOGGER.info("Retrieving user {} ", id);
+        LOGGER.info("Retrieving user with id: {} ", id);
 
         return userRepository.findById(id).orElseThrow(()
                 -> new ResourceNotFoundException("User " + id + " does not exist"));
 
+    }
+    public User updateUser(long id, SaveUserRequest request) {
+        LOGGER.info("Updating user : {}: {} ", id, request);
 
+        User existingUser = getUser(id);
+
+        BeanUtils.copyProperties(request, existingUser);
+
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUser(long id) {
+        LOGGER.info(" Deleting user : {}", id);
+
+       userRepository.deleteById(id);
     }
 }
